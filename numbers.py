a73b70073ce7c7fb98589f92e3856e6ec6abb11d
DollyVadaliya <140232322+DollyVadaliya@users.noreply.github.com>
2023-08-05 11:26:17 +0530

    Add files via upload

---
+++ numbers.py
@@ -0,0 +1,19 @@
+a = str('20')
+b = int(75)
+c = complex(8j)
+d = int(8.98)
+
+print(type(a))   
+print(type(b))
+print(type(c))
+print(type(d))
+
+print(a)    
+print(b)
+print(c)
+print(d)
+
+import random
+
+print(random.randrange(1, 45678))
+
